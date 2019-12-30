c0f329b9e00c243bb24ce769d6c0ca01f7402f8e
radiosmersh <radiosmersh@gmail.com>
2019-12-30 15:10:47 +0300

    Update package.json and obsolete link

--- app/wrong_os.js
+++ app/wrong_os.js
@@ -23,7 +23,7 @@ $(() => {
     }
 
     function onGithubLinkPress() {
-        shell.openExternal('https://github.com/kutuzovrusss/FH2Download')
+        shell.openExternal('https://github.com/radiosmersh/FH2Download')
     }
 
     document.querySelector('#close-button').addEventListener('click', onCloseButtonPress);
